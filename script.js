c895a8a71aa69aec7bc010e1e2e5b08d731c976f
Harish  Parab <50201375+HarishPrb28@users.noreply.github.com>
2023-02-21 14:35:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here
+let num = document.getElementById("num")
+let respond = document.getElementById("respond")
+let rnum = Math.floor(Math.random()*100)
+	console.log(rnum)
+function guessNum(){
+let guess = document.getElementById("guess").value
+
+	if(guess == rnum){
+		num.innerHTML = `You guessed right, your number was ${rnum}`
+	}
+	else if(guess < rnum){
+		respond.innerHTML ="Hot"
+	}
+	else if(guess > rnum){
+		respond.innerHTML ="Cold"
+	}	
+	else {
+		num.innerHTML = "Invalid Number"
+	}
+}
+	
+
 
